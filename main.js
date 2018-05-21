1eaaea81451b14b60a0b07ecc0439690135f8f82
Aswinikumar02 <aswinikumar45@yahoo.com>
2018-05-21 17:41:21 +0530

    comment removed

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ var http = require("http");
 http.createServer(function (request, response) {
 
    
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
+   
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
